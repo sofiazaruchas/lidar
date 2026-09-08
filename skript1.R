c677bcfc404b0e99abe9c2ba2d881ede121b35f6
sofiazaruchas <zarso@web.de>
2026-09-08 13:31:03 +0200

    skript

---
+++ skript1.R
@@ -0,0 +1,27 @@
+# load libraries
+library(lidR)
+library(terra)
+library(sf)
+
+#load pointcloud
+als_dir <- "C:/Users/Sofia Zaruchas/OneDrive/Desktop/EAGLE/lidar/exam/data/als"
+ctg <- readLAScatalog(als_dir)
+
+print(ctg)
+plot(ctg)
+
+#create output path
+dir.create("data/als_normalized", showWarnings = FALSE)
+opt_output_files(ctg) <- "data/als_normalized/{*}_norm"
+
+#inspectation of single las tile
+las <- readLAS(list.files(als_dir, pattern = "\\.laz$", full.names = TRUE)[1])
+plot(las, color = "ScanAngleRank", axis = TRUE, legend = TRUE)
+las_check(las)
+
+#Normalization of data
+ctg_norm <- normalize_height(ctg, algorithm = knnidw())
+
+list.files("data/als_normalized")
+
+
