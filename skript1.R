442343026846a5e24f503f65099b31e75604af06
sofiazaruchas <zarso@web.de>
2026-09-11 11:19:05 +0200

    metrics and canopy height model

--- skript1.R
+++ skript1.R
@@ -24,6 +24,23 @@ ctg_norm <- normalize_height(ctg, algorithm = knnidw())
 
 list.files("data/als_normalized")
 
-ctg_norm2 <- readLAScatalog("data/als_normalized")
+ctg_norm2 <- readLAScatalog("data/als_normalized", filter = "-drop_z_above 60 -drop_z_below -5")
 
+#calculation of metrics
+metrics <- pixel_metrics(ctg_norm2, func = .stdmetrics_z, res = 10)
+print(metrics)
 
+#visualisation of metrics
+plot(metrics, "zsd", col = height.colors(50))
+plot(metrics, "zmean", col = height.colors(50))
+
+#canopy height model
+opt_output_files(ctg_norm2) <- ""
+chm <- rasterize_canopy(
+  ctg_norm2,
+  res = 1,
+  algorithm = pitfree(thresholds = c(0, 2, 5, 10, 15), max_edge = c(0, 1.5))
+)
+
+print(chm)
+plot(chm, col = height.colors(50))
